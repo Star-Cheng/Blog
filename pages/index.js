615f2663b26e15d0db3db3e762483a2035a4358c
Gym <1335929010@qq.com>
2024-10-20 15:10:47 +0800

    更新头像

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function HomePage() {
+  return (
+    <div className={styles.container}>
+      <h1>欢迎来到我的主页</h1>
+      <div className={styles.avatarWrapper}>
+        <Image
+          src="/images/avatar.png"
+          alt="我的头像"
+          width={200}
+          height={200}
+          layout="responsive"
+          className={styles.avatar}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
